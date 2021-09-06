2e916712feb1bef1e1aa8e27e127a2dff94082ee
subineejung <90013966+subineejung@users.noreply.github.com>
2021-09-06 15:39:32 +0900

    20210906 update
    이미지 갤러리 실습
    나

---
+++ 01_web_html/05_html_summary.php
@@ -0,0 +1,18 @@
+<!-- -->
+
+
+<html>
+    <title>나의 웹 페이지</title>
+	<body>
+    <a href="https://google.co.kr">구글</a><br>
+	<a href="https://naver.com">네이버</a>
+	
+	<h1>내가 좋아하는 동물</h1><br>
+	<a href="https://unsplash.com/photos/Sg3XwuEpybU">래브라도리트리버</a><br>
+	<img src="oscar.jpg" width=300 height=300 alt="not found"
+	<img src="C:\Users\Subin Jung\Desktop" width=300 height=300 alt="not found
+	<p title="I'm a tooltip> This is a paragraph. </p>
+	<img src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1824&q=80 style=" width=300 height =300" alt="not found">	
+	</body>
+<html>
+
